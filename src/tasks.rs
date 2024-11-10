4f0efc18fe7315653de643de475ae0e8f8e169f0
abekem <nek.7180@gmail.com>
2024-11-10 07:20:39 +0000

    タスク一覧のコンストラクタを追加

--- src/tasks.rs
+++ src/tasks.rs
@@ -1,17 +1,42 @@
 use crate::task::Task;
 
-struct Tasks {
+pub struct Tasks {
     // フィールドの定義
     tasks: Vec<Task>,
 }
 
 impl Tasks {
+    // コンストラクタ
+    fn new(tasks: Vec<Task>) -> Self {
+        Self { tasks }
+    }
+
+    // タスク一覧を表示する
     pub fn show(&self) -> String {
         self.tasks
             .iter()
             .map(|task| task.show())
             .collect::<Vec<String>>()
             .join("\n")
+    }
+
+    // タスク一覧を返す
+    pub fn list() -> Tasks {
+        // 初期化したTasks構造体を返す
+        let task_a = Task::new(
+            1,
+            "未完了".to_string(),
+            "A".to_string(),
+            "description of task A".to_string(),
+        );
+        let task_b = Task::new(
+            2,
+            "実施中".to_string(),
+            "B".to_string(),
+            "description of task B".to_string(),
+        );
+
+        Tasks::new(vec![task_a, task_b])
     }
 }
 
